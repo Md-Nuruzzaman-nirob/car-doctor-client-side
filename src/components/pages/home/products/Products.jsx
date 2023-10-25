ba96c9bf0f88c06f17c1d83df5a122f6c759ae65
Md-Nuruzzaman-Nirob <info.mdnuruzzaman1@gmail.com>
2023-10-25 15:36:18 +0600

    update the project

--- src/components/pages/home/products/Products.jsx
+++ src/components/pages/home/products/Products.jsx
@@ -1,6 +1,6 @@
 const Products = () => {
   return (
-    <div>
+    <div className="mb-20">
       <div className="space-y-3 lg:space-y-5 text-center">
         <p className="text-xl font-bold text-[#FF3811]">Popular Products</p>
         <h3 className="text-3xl lg:text-4xl xl:text-5xl font-bold">
